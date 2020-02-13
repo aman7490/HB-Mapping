7ede982651eac0ab9c26046f628c16376d470fec
amandeep.singh <amandeep.singh@VLHRL-PBTRARD.vlinkinfo.com>
2020-02-13 21:14:02 +0530

    COMMIT

---
+++ HB-Mapping/src/main/java/com/hbmapping/HBMapping/HbMappingApplication.java
@@ -0,0 +1,13 @@
+package com.hbmapping.HBMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbMappingApplication.class, args);
+	}
+
+}
---
+++ HB-Mapping/src/test/java/com/hbmapping/HBMapping/HbMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbmapping.HBMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
